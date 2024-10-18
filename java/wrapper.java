96594e4cead5ebb6963304b3b19ae6b5463fd8a3
nithieshh <nithieshk80@gmail.com>
2024-10-18 22:24:32 +0530

    Update wrapper.java

--- java/wrapper.java
+++ java/wrapper.java
@@ -1,19 +1,17 @@
-//wrapper class program
-
 import java.util.*;
-public class wrapper {
-   public static void main(String[]args)
-  {
-    wrapper x = new wrapper();
-    wrapper b = new wrapper();
-    x = null;
-    b = null;
-    new wrapper();
-    System.gc();
-  }
-  public void finalize()
-  {
-    System.out.print("this is GC ");
 
-  }
+public class WrapperClass {
+   public static void main(String[] args) {
+      WrapperClass x = new WrapperClass();
+      WrapperClass b = new WrapperClass();
+      x = null;
+      b = null;
+      new WrapperClass();
+      System.gc();  // Requesting Garbage Collection
+   }
+
+   @Override
+   protected void finalize() {
+      System.out.print("this is GC ");
+   }
 }
