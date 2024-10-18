95e4468251753461d5d9afa196b9336ad5c952ef
nithieshh <nithieshk80@gmail.com>
2024-10-18 22:15:27 +0530

    Update 3sum.java

--- java/3sum.java
+++ java/3sum.java
@@ -3,49 +3,43 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSum {
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+
+        // If the input has less than 3 elements, return empty result
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        // Sort the array to facilitate the two-pointer technique
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates for the first element
             if (i > 0 && nums[i] == nums[i - 1]) {
-                continue; // Skip duplicates
+                continue;
             }
+
+            // Use two pointers to find the remaining two numbers
             int target = -nums[i];
             int left = i + 1;
             int right = nums.length - 1;
 
             while (left < right) {
                 int sum = nums[left] + nums[right];
+
                 if (sum == target) {
+                    // Found a triplet
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
                     left++;
                     right--;
 
+                    // Skip duplicates for the second and third elements
                     while (left < right && nums[left] == nums[left - 1]) {
-                        left++; // Skip duplicates
+                        left++;
                     }
                     while (left < right && nums[right] == nums[right + 1]) {
-                        right--; // Skip duplicates
-                    }
-                } else if (sum < target) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-        ThreeSum solution = new ThreeSum();
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> result = solution.threeSum(nums);
-
-        for (List<Integer> triplet : result) {
-            System.out.println(triplet);
-        }
-    }
-}
+                        right--;
+                   
